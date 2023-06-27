8157d120110a0672f1c7fe5149901c3ae7c96335
Bitzilla <luca@kelepar.com>
2023-06-27 17:39:28 +0800

    add service workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,6 +7,9 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
+// service workers
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 module.exports = {
     mode: "production",
     entry: './src/client/index.js',
@@ -39,7 +42,8 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new MiniCssExtractPlugin({ filename: "[name].css" })
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
 
